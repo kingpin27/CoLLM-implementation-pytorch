65dbd64442eab6b74956715556ca0df2df6144d9
kingpin27 <anishchaudhary2706@gmail.com>
2026-03-19 05:10:52 +0530

    FIX: repeated param bug

--- train.py
+++ train.py
@@ -589,7 +589,6 @@ if __name__ == "__main__":
         projection_dim=768,
         projection_dropout=0.0,
         disable_last_layers=4,
-        llm_trainable_last_layers=10,
         llm_block_path="model.language_model.layers",
         llm_trainable_last_layers=10,
     )
