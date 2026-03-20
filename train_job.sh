17ed25c213a382a13248c68381371c12a118dfa3
kingpin27 <anishchaudhary2706@gmail.com>
2026-03-21 02:49:28 +0530

    UPDATE: upgrade node specs

--- train_job.sh
+++ train_job.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=bash
 #SBATCH --partition=ada
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
 #SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00
 #SBATCH --output=slurm-%j.out
